b6f52aa0b80a2ce2ce36e2924016742dc19bf421
ani-codes24 <tapase.aniruddha.0@gmail.com>
2025-12-23 23:06:01 +0530

    Added option to add and save tasks and some logical part of it

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [todolist,settodolist]=useState([])
+  let saveToDoList=(event)=>{
+
+    let todoname=event.target.todoname.value;
+
+    if(!todolist.includes(todoname)){
+      let finaltodo=[...todolist,todoname]
+      settodolist(finaltodo)
+    }
+    else{
+      alert("Already Exists!!")
+    }
+    event.preventDefault();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> To-Do List</h1>
+      <form onSubmit={saveToDoList}>
+        <input type = "text" name='todoname'/> <button>Save</button>
+      </form>
+      <div className='outerDiv'>
+        <ul>
+            <li>JavaScript <span>&times;</span></li>
+        </ul>
+      </div>
     </div>
   );
 }
