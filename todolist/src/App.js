4ed85d846b3df36d1723e88613f9701026d7651e
ani-codes24 <tapase.aniruddha.0@gmail.com>
2025-12-26 04:08:58 +0530

    Completed by doing logical part of task adding, deleting and striking through it

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -20,12 +20,16 @@ function App() {
   }
   let list=todolist.map((value, index)=>{
     return(
-      <ToDoListItems/>
+      <ToDoListItems value={value} key={index} indexNumber={index} todolist={todolist} settodolist={settodolist}/>
     )
   })
   return (
     <div className="App">
       <h1> To-Do List</h1>
+      <p>Welcome to ToDo List!!<br/>
+        Add Tasks You Want to Do<br/>
+        Click on x to Remove a Task<br/>
+        Click on Task to Mark it Complete!</p>
       <form onSubmit={saveToDoList}>
         <input type = "text" name='todoname'/> <button>Save</button>
       </form>
@@ -39,8 +43,16 @@ function App() {
 }
 
 export default App;
-function ToDoListItems(){
+function ToDoListItems({value,indexNumber,todolist,settodolist}){
+  let [status,setStatus]=useState(false)
+    let deleteRow=()=>{
+      let finalData=todolist.filter((v,i)=>i!=indexNumber)
+      settodolist(finalData)
+    }
+    let checkStatus=()=>{
+      setStatus(!status)
+    }
   return(
-    <li>JavaScript <span>&times;</span></li>
+    <li className={(status)? 'completetodo': ''} onClick={checkStatus}>{indexNumber+1}. {value}<span onClick={deleteRow}>&times;</span></li>
   )
 }
